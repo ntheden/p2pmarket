4de87678d4b4c80cc6690f604f359be27a949892
Nicolas Pulido <pulido@zonehacks.net>
2022-08-31 14:54:48 -0700

    begin db support

--- api/main.py
+++ api/main.py
@@ -8,11 +8,13 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse, FileResponse
 from pathlib import Path
 from pyrogram import Client as TelegramClient
-from typing import Union
+from sqlmodel import SQLModel, create_engine, Field, Session
+from typing import Union, Optional
 import uvloop
 import uvicorn
 
 
+# configure logging
 logs = Path('.').joinpath('logs')
 logs.mkdir(exist_ok=True)
 logging.config.fileConfig(
@@ -20,6 +22,7 @@ logging.config.fileConfig(
         defaults={'logfilename': logs/'api.log'},
         disable_existing_loggers=False
 )
+# for pyrogram speedup
 uvloop.install()
 
 
@@ -45,24 +48,81 @@ async def progress(current, total):
     print(f"{current * 100 / total:.1f}%")
 
 
-async def download_media(tg, message):
+def db_get_media(msg_dict):
+    '''
+    TODO: get info from db and set to this dict
+    '''
+    msg_dict["media_type"] = None
+    return msg_dict
+
+
+class MessageMedia(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    msg_id: int = Field(index=True)
+    path: Union[str, None]
+    thumb_path: Union[str, None]
+    type: str = "photo"
+
+
+# configure database
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+engine = create_engine(sqlite_url, echo=True, future=True)
+SQLModel.metadata.create_all(engine)
+
+
+def db_set_media(msg_dict):
+    media_dict = msg_dict[msg_dict["media_type"]]
+    if not media_dict:
+        return msg_dict
+    with Session(engine) as session:
+        media = MessageMedia(
+            msg_id=msg_dict['id'],
+            path=media_dict["file_path"],
+            thumb_path=media_dict["thumb_path"],
+            type=msg_dict["media_type"],
+        )
+        print(media)
+        session.add(media)
+        session.commit()
+    return msg_dict
+
+
+async def download_and_update_media(tg, message) -> dict:
+    msg_dict = json.loads(str(message))
+    del msg_dict['chat'] # remove redundant field
     files = Path(f"downloads/{message.id}")
+    # TODO: check both db and filesystem
+    msg_dict = db_get_media(msg_dict)
     if files.is_dir() and any(files.iterdir()):
         # already something there. can't verify they are all there or file
         # integrity, good enough though
         if not Flags.redownload:
-            return
+            return msg_dict
     media = message.photo or message.video
     if not media:
-        # there are many other types of media, only interested in these two for now
-        return
+        # there are many other types of media, only interested in these two
+        return msg_dict
+    key = "photo" if message.photo else "video"
+    msg_dict["media_type"] = key
+    media_dict = msg_dict[key]
+    media_dict.update({
+        "file_path": None,
+        "thumb_path": None,
+    })
     fpath = await tg.download_media(message, f"downloads/{message.id}/", progress=progress)
-    if fpath and media.thumbs:
+    if not fpath:
+        return msg_dict
+    media_dict["file_path"] = fpath
+    if media.thumbs:
+        thumb_dict = media_dict["thumbs"][0]
         fpath = Path(fpath)
+        thumb_dict["file_path"] = fpath.parent.joinpath('thumb-'+fpath.name)
         thumb = await tg.download_media(
             media.thumbs[0].file_id,
-            fpath.parent.joinpath('thumb-'+fpath.name)
+            thumb_dict["file_path"],
         )
+    return db_set_media(msg_dict)
 
 
 @app.on_event("startup")
@@ -77,23 +137,28 @@ async def synch_messages(chat_id: str = None):
     logger.info("Synchronizing Telegram Messages..")
     async with TelegramClient("my_account") as tg:
         async for message in tg.get_chat_history(chat_id):
-            await download_media(tg, message)
-            messages.append(message)
+            messages.append({
+                'obj': message,
+                'dict': await download_and_update_media(tg, message),
+            })
     logger.info("Done Synchronizing Telegram Messages")
 
 
 @app.get("/telegram/{chat_id}")
-async def get_message(chat_id: str, msg_id: Union[int, None] = None, thumb: Union[bool, None] = None):
-    logger = logging.getLogger("main")
+async def get_message(
+        chat_id: str,
+        msg_id: Union[int, None] = None,
+        thumb: Union[bool, None] = None
+    ) -> Union[FileResponse, dict]:
     global messages
     if not messages:
         await synch_messages(chat_id)
     if not msg_id:
-        return sorted([message.id for message in messages])
-    msg = [m for m in messages if m.id == msg_id]
-    msg = msg[0] if msg else None
+        return sorted([message['obj'].id for message in messages])
+    msg = [m for m in messages if m['obj'].id == msg_id]
+    msg = msg[0]['obj'] if msg else None
     if not thumb:
-        return json.loads(str(msg)) if msg else {}
+        return msg_dict
     fpath = Path(f"downloads/{msg.id}")
     if fpath.is_dir() and any(fpath.iterdir()):
         # If more than one, the order will likely be first in at
