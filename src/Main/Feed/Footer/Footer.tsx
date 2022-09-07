9f61e00ed8d9711f8b324ca53a7ca123ce986e22
Nicolas Pulido <pulido@zonehacks.net>
2022-09-06 23:55:44 -0700

    make footer a button

--- src/Main/Feed/Footer/Footer.tsx
+++ src/Main/Feed/Footer/Footer.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
-import { Card, Form } from 'react-bootstrap';
+import { Card, Form, Button } from 'react-bootstrap';
 import { BsTelegram } from 'react-icons/bs';
 
 import styles from './Footer.module.scss';
 
 export const Footer = () => (
     <Card.Footer className={styles.footer}>
-        <BsTelegram />
-        <Form.Control
-            type="text"
-            placeholder="Contact Counterparty..."
-            className={styles.addCommentFormInput}
-        />
+        <Button variant="outline-success" id={styles.button}>
+            Take Offer
+        </Button>
     </Card.Footer>
 );
