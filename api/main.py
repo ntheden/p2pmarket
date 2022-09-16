66783025397b10630d78f7be1c910b997b7dcc51
Nicolas Pulido <pulido@zonehacks.net>
2022-09-15 17:21:24 -0700

    update default app path

--- api/main.py
+++ api/main.py
@@ -19,12 +19,13 @@ import uvloop
 import uvicorn
 
 
-app_path = '.' # see Dockerfile
+default_app_path = Path(__file__).parent
 env = Env(expand_vars=True)
-env.read_env(".env")
+env.read_env(default_app_path/"main.env")
+default_app_path = default_app_path/"run"
 # configure logging
 with env.prefixed('P2PSTORE_'):
-    base = Path(env('PATH', app_path))
+    base = Path(env('PATH', default_app_path))
     logs = base.joinpath('logs')
 logs.mkdir(parents=True, exist_ok=True)
 logging.config.fileConfig(
@@ -178,7 +179,7 @@ class Message(SQLModel, table=True):
 
 # configure database
 with env.prefixed('P2PSTORE_'):
-    db_path = Path(env('PATH', app_path)).joinpath(env('DB_NAME', 'database.db'))
+    db_path = Path(env('PATH', default_app_path)).joinpath(env('DB_NAME', 'database.db'))
 sqlite_url = f"sqlite:///{db_path}"
 engine = create_engine(sqlite_url, echo=True, future=True)
 SQLModel.metadata.create_all(engine)
@@ -232,7 +233,7 @@ async def download_and_update_media(tg, message) -> dict:
     msg_dict = json.loads(str(message))
     del msg_dict['chat'] # remove redundant field
     with env.prefixed('P2PSTORE_'):
-        files = Path(env('PATH', app_path))/f"downloads/{message.id}/"
+        files = Path(env('PATH', default_app_path))/f"downloads/{message.id}/"
     # TODO: check both db and filesystem
     msg_dict = db_get_media_old(msg_dict)
     if files.is_dir() and any(files.iterdir()):
@@ -295,7 +296,7 @@ async def db_set_user(session, usr, tg):
     media = user.media[0]
     media.path = f"downloads/users/{user.id}/"
     with env.prefixed('P2PSTORE_'):
-        dest = Path(env('PATH', app_path))/media.path
+        dest = Path(env('PATH', default_app_path))/media.path
     dpath = await tg.download_media(usr.photo.big_file_id, f"{dest}/")
     media.name = Path(dpath).name
     if usr.photo.small_file_id:
@@ -315,7 +316,7 @@ async def db_set_media(session, msg, container_msg, tg):
     db_media.type = "photo" if msg.photo else "video"
     db_media.path = f"downloads/messages/{msg.id}/"
     with env.prefixed('P2PSTORE_'):
-        dest = Path(env('PATH', app_path))/db_media.path
+        dest = Path(env('PATH', default_app_path))/db_media.path
     fpath = await tg.download_media(msg, f"{dest}/")
     if not fpath:
         return db_media
@@ -482,7 +483,7 @@ async def get_message(
     if not media:
         return FileResponse("static/no-image.jpg")
     with env.prefixed('P2PSTORE_'):
-        fpath = Path(env('PATH', app_path))/media[0].path
+        fpath = Path(env('PATH', default_app_path))/media[0].path
     fpath = fpath/f"thumb-{media[0].name}"
     if not fpath.is_file():
         return FileResponse("static/no-image.jpg")
@@ -505,7 +506,7 @@ async def get_media(
     except sqlalchemy.exc.NoResultFound:
         return FileResponse("static/no-image.jpg")
     with env.prefixed('P2PSTORE_'):
-        fpath = (Path(env('PATH', app_path))/media.path)/name
+        fpath = (Path(env('PATH', default_app_path))/media.path)/name
     if (fpath).is_file():
         return FileResponse(fpath)
     return FileResponse("static/no-image.jpg")
@@ -528,7 +529,7 @@ async def get_message_old(
     if not thumb and not photo:
         return msg['dict']
     with env.prefixed('P2PSTORE_'):
-        fpath = Path(env('PATH', app_path))/f"downloads/{msg['obj'].id}/"
+        fpath = Path(env('PATH', default_app_path))/f"downloads/{msg['obj'].id}/"
     if fpath.is_dir() and any(fpath.iterdir()):
         thumb_name = None
         path = None
