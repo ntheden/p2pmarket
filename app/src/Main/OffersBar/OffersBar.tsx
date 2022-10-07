f0584728adb86cdbe3202fc186d1ab9b77c7fa17
Nicolas Pulido <pulido@zonehacks.net>
2022-10-07 15:03:55 -0700

    arrange the order of the carousel

--- app/src/Main/OffersBar/OffersBar.tsx
+++ app/src/Main/OffersBar/OffersBar.tsx
@@ -43,12 +43,51 @@ export const OffersBar = (props: FuncProps): JSX.Element => {
         setCarouselIndex(carouselIndex < maxIndex ? carouselIndex + 1 : carouselIndex);
     }
 
+    const filter = (ar: Array<number>) => {
+        // TODO: filter out stuff that doesn't match, for instance:
+        // 1. has picture
+        // 2. has word on the user's banned list. There will
+        //    be a default short list of banned words that the user
+        //    can modify
+        const filters = localStorage.getItem('carousel.filters');
+        console.log("filters", filters);
+        return ar;
+    }
+
     const shuffle = (o: Array<number>) => {
       for (var j, x, i = o.length;
            i;
            j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x
       );
       return o;
+    }
+
+    const arrange = (ar: Array<number>) => {
+        enum eArrangement {
+            Shuffle = "Shuffle",
+            Newest = "Newest",
+            Oldest = "Oldest",
+        }
+        let arrangement = localStorage.getItem('carousel.arrangement');
+        if (arrangement === null) {
+            localStorage.setItem('carousel.arrangement', eArrangement.Shuffle);
+            arrangement = eArrangement.Newest;
+        }
+        console.log("arrangement", arrangement);
+        switch(arrangement) {
+            case eArrangement.Shuffle:
+                return shuffle(ar);
+            case eArrangement.Newest:
+                return ar.sort((first: number, second: number) => {
+                    return (first > second ? -1 : 1)
+                })
+            case eArrangement.Oldest:
+                return ar.sort((first: number, second: number) => {
+                    return (first > second ? 1 : -1)
+                })
+        }
+        // default
+        return shuffle(ar);
     }
 
     const chunks = (ar: Array<number>) => {
@@ -58,7 +97,7 @@ export const OffersBar = (props: FuncProps): JSX.Element => {
         setMaxIndex(count);
         for (let i=0; i<count; i++) {
             ar1.push(ar.splice(0, 6).map((item, index) => (
-              <ListGroup.Item key={i+index} onClick={() => carouselSelection(item)}>
+              <ListGroup.Item key={(i*6)+index} onClick={() => carouselSelection(item)}>
                 {item === undefined ? (
                     <Spinner animation="border" role="status">
                         <span className="visually-hidden">Loading...</span>
@@ -88,9 +127,9 @@ export const OffersBar = (props: FuncProps): JSX.Element => {
     }
 
     useEffect(() => {
-        // random initial selection FIXME: pick from current carouselIndex
-        carouselSelection(allOfferIds[Math.floor(Math.random() * allOfferIds.length)])
-        setCarouselItems(listChunks(chunks(shuffle(allOfferIds))));
+        const msgIds = filter(arrange(allOfferIds));
+        carouselSelection(msgIds[0]);
+        setCarouselItems(listChunks(chunks(msgIds)));
     }, [allOfferIds]);
 
     return (
